39e9c8d80db75a79153971ca73455c3402200d37
Dneifiend <dneifiend@gmail.com>
2022-07-21 16:58:45 +0900

    core 구현

--- main.js
+++ main.js
@@ -1,8 +1,77 @@
-var data = window.location.pathname.slice(4).split(",").at(-1)
-var dataUrl  ="data:application/octet-stream;base64,"+decodeURIComponent(data)
+var d = {
+    flag:true,
+    get l(){return d.flag?console.log:()=>{}}
+}
 
-fetch(dataUrl,{type:"text/plain"})
-.then(res=>res.text())
-.then(text=>{
-    document.querySelector('.body').innerHTML = text
-})
+
+document.onreadystatechange = ()=>{
+    if(document.readyState !== 'complete'){
+        return
+    }
+    
+    console.log('DOM loaded')
+    var data = window.location.search.slice(1).split(",").at(-1)
+
+    if(data.length===0){
+        // menu 표기
+        document.querySelector('.menu').classList.remove('hidden')
+
+
+    }
+    else{
+        d.l(data,'found data')
+        // 데이터 표기
+        document.querySelector('.body').classList.remove('hidden')
+
+        var dataUrl  ="data:application/octet-stream;base64,"+decodeURIComponent(data)
+        
+        fetch(dataUrl,{type:"text/plain"})        
+        .then(res=>res.text())
+        .then(text=>{
+            document.querySelector('.body').innerHTML = text
+        })
+    
+    }
+}
+
+
+
+function readDataUrl(){
+    return new Promise((res,rej)=>{
+        var input = document.getElementById('plain-data').value
+        if(input.length === 0 ){
+            rej('')
+        }
+
+
+        var blob = new Blob([input])
+    
+        var reader = new FileReader()
+        reader.addEventListener('loadend',ev=>{
+            res(ev.target.result.split(",").at(-1))
+        })
+        reader.readAsDataURL(blob)
+    })
+}
+
+let removeChild = (element)=>{
+    let childEles = element.querySelectorAll("*")
+    childEles?.forEach(child=>child.remove())
+}
+
+async function qrCode(){
+    let qrcodeContainer = document.querySelector('#qrcode-container')
+    removeChild(qrcodeContainer)
+    var data = window.location.pathname +"?"+ await readDataUrl()
+    console.log(data)
+
+    var qrcode = new QRCode(qrcodeContainer,{
+        text: data,
+        width: 600,
+        height: 600,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    })
+    qrcode.makeCode(data)
+}
